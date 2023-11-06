679284c443b5f15cf903e32f26d62851cc40f846
J. Jaime Paredes <jjpp109309@gmail.com>
2023-11-05 18:45:35 -0600

    assertwandb version

--- ci.py
+++ ci.py
@@ -2,4 +2,4 @@ import wandb
 
 print(f'The version of wandb is {wandb.__version__}')
 version = wandb.__version__
-assert version == "2.2", 'Expected 2.2 got {}'.format(version)
+assert version == "0.15.12", 'Expected 2.2 got {}'.format(version)
