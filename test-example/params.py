0bc1e55c4700e68cbe1f076b99bd9bf3447093df
J. Jaime Paredes <jjpp109309@gmail.com>
2023-11-06 20:17:08 -0600

    remove entity

--- test-example/params.py
+++ test-example/params.py
@@ -1,5 +1,4 @@
 WANDB_PROJECT = "mlops-course-001"
-ENTITY = 'janinos'
 BDD_CLASSES = {i:c for i,c in enumerate(['background', 'road', 'traffic light', 'traffic sign', 'person', 'vehicle', 'bicycle'])}
 RAW_DATA_AT = 'av-team/mlops-course-001/bdd_simple_1k'
 PROCESSED_DATA_AT = 'av-team/mlops-course-001/bdd_simple_1k_split'
