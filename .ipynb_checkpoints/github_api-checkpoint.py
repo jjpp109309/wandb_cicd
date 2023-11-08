4eaa852d279b0b469be66b1a84dffaa2d882695f
J. Jaime Paredes <jjpp109309@gmail.com>
2023-11-08 15:49:15 -0600

    update dependenncies

---
+++ .ipynb_checkpoints/github_api-checkpoint.py
@@ -0,0 +1,5 @@
+import os
+from ghapi.core import GhApi
+
+api = GhApi(owner='jjpp109309', repo='wandb_cicd')
+
--- ci.py
+++ ci.py
@@ -2,4 +2,4 @@ import wandb
 
 print(f'The version of wandb is {wandb.__version__}')
 version = wandb.__version__
-assert version == "0.15.12", 'Expected 2.2 got {}'.format(version)
+assert version == "0.16.0", 'Expected 0.16.0 got {}'.format(version)
